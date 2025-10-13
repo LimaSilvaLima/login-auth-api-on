12ed5dadb41f5eaa269f804431d5dfb94e0fc518
LimaSilvaLima <147210553+LimaSilvaLima@users.noreply.github.com>
2025-10-13 15:52:47 -0300

    rodando SecurityFilter

--- src/main/java/com/login_auth_api/repositories/UserRepository.java
+++ src/main/java/com/login_auth_api/repositories/UserRepository.java
@@ -1,11 +1,13 @@
 package com.login_auth_api.repositories;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.login_auth_api.domain.User;
 
 public interface UserRepository  extends JpaRepository<User, String> {
-    User findByEmail(String email);
+    Optional<User> findByEmail(String email);
     User findByEmailAndPassword(String email, String password);
 
 }
